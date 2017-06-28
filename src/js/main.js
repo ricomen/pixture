65a943d71e06e07cf15538e65d0693be0dd8ac68
Khamitov Timur <ricomen@mail.ru>
2017-06-28 16:48:31 +0500

    add gsap library

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,43 @@
 //jQuery begin
 $(document).ready(function() {
 
+  $(".slider-js").slick({
+    infinite: true,
+    slidesToShow: 3,
+    centerMode: true,
+    nextArrow: "slider-control-js",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1        
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });  
+
 });
-$(window).load(function() {
-  /* Act on the event */
-});
-//jQuery end
-(function($){
-  
-})(jQuery);
+  var tl = new TimelineMax();  
+  tl
+  .fromTo('.section__logo', 1, { y: -300, opacity: 0}, {y: 0, opacity: 1})
+  .fromTo('.page-header__mail', 0.7, { y: -100, opacity: 0}, {y: 0, opacity: 1})
+  .fromTo('.section__bar', 0.5, { x: -100, opacity: 0}, {x: 0, opacity: 1})
+
+
+
+
